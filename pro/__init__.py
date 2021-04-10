fcbfbc3c458e59774ac8f0e40ce5fd0604fb0702
Chen Yi <76426561+chenyi14501@users.noreply.github.com>
2021-04-10 13:27:25 +0800

    add __init__.py  for import

---
+++ pro/__init__.py
@@ -0,0 +1,10 @@
+'''
+Author: your name
+Date: 2021-03-17 11:22:31
+LastEditTime: 2021-03-28 14:50:23
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: /builder/util/__init__.py
+'''
+from . import pro_map
+from . import trans
